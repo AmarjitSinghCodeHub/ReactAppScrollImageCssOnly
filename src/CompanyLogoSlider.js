76910372350a48adf8a3572babe982c570526d83
AmarjitSinghCodeHub <amarjitsinghgigs@gmail.com>
2024-09-21 11:34:35 +0530

    First Commit

---
+++ src/CompanyLogoSlider.js
@@ -0,0 +1,14 @@
+import React from "react";
+const CompanyLogoSlider=()=>{
+    return(
+        <>
+        <div className="slider-content">
+        <img src="https://images.unsplash.com/photo-1534528741775-53994a69daeb?q=80&w=1528&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="1"/>
+        <img  src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="2"/>
+        <img   src="https://plus.unsplash.com/premium_photo-1671656333460-793292581bc6?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="3"/>
+        <img  src="https://images.unsplash.com/photo-1441786485319-5e0f0c092803?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="4"/>
+      </div>
+        </>
+    )
+}
+export default CompanyLogoSlider
---
+++ src/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import CompanyLogoSlider from "./CompanyLogoSlider";
+
+const Home=()=>{
+    return(
+        <>
+          <CompanyLogoSlider/>
+        </>
+    )
+}
+export default Home
